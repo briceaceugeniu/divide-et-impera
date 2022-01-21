c39177a22f737792a8a19a22315a63e9d60c9125
Eugeniu <28739569+briceaceugeniu@users.noreply.github.com>
2022-01-21 17:56:34 +0100

    bug fix: set meaningful variable name

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -7,14 +7,14 @@ import SupertasksList from "./components/supertasks/SupertasksList";
 function App() {
   const [tasksCount, setTasksCount] = useState(1);
 
-  let longtext = [];
+  let justForTestingPurposes = [];
 
   function handleAddTask() {
     setTasksCount(tasksCount + 1);
   }
 
   for (let i = 0; i < tasksCount; i++) {
-    longtext.push(
+    justForTestingPurposes.push(
       <TaskCard
         key={i}
         title={"Clickable" + i}
@@ -33,7 +33,7 @@ function App() {
       <div className="nav demo">
         <div>Navbar</div>
       </div>
-      <div className="task-tree demo">{longtext}</div>
+      <div className="task-tree demo">{justForTestingPurposes}</div>
       <div className="task-list demo">
         <SupertasksList />
       </div>
